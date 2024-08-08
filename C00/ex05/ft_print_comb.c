33726d98e0107115de49330b2a57ebc4b5a8e4fd
elfo <elfo@Elfo.myguest.virtualbox.org>
2024-08-08 23:48:12 +0200

    ex5 en proceso

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -6,40 +6,50 @@
 /*   By: egelma-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 15:17:27 by egelma-b          #+#    #+#             */
-/*   Updated: 2024/08/08 17:16:50 by egelma-b         ###   ########.fr       */
+/*   Updated: 2024/08/08 23:47:33 by elfo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
 void	ft_print_comb(void)
 {
-	char numero[5];
-	for(int i = 0; i<10;i++)
+	int	i;
+	int	j;
+	int	k;
+	char	characters[5];
+
+	i = 0;
+	while (i < 10)
 	{
-		for(int j = i+1; j < 10; j++)
+		j = i + 1;
+		while (j < 10)
 		{
-			for(int k = j+1; k < 10; k++)
+			k = j + 1;
+			while (k < 10)
 			{
-				numero[0] = '0' + i;
-				numero[1] = '0' + j;
-				numero[2] = '0' + k;
-				if(i != 7)
+				characters[0] = '0' + i;
+				characters[1] = '0' + j;
+				characters[2] = '0' + k;
+				characters[3] = ',';
+				characters[4] = ' ';
+				if (i != 7)
 				{
-					numero[3] = ',';
-					numero[4] = ' ';
-					write(1, numero, 5);
+					write(1, characters, 5);
 				}
 				else
 				{
-					write(1, numero, 3);
+					write(1, characters, 3);
 				}
+				k++;
 			}
+			j++;
 		}
+		i++;
 	}
 }
 
 int	main(void)
 {
 	ft_print_comb();
-	return(0);
+	return (0);
 }
